04a618cc6fb31c49478821ff99de91fbc1397fe7
David J Nevin <66497218+davidjnevin@users.noreply.github.com>
2023-01-23 23:55:33 +0100

    Add setup_files.sh

---
+++ setup_files.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Set the urls to be passed to the curl commands
+
+curl -O https://raw.githubusercontent.com/davidjnevin/python_automation/main/reports.py
+curl -O https://raw.githubusercontent.com/davidjnevin/python_automation/main/changeImage.py
+curl -O https://raw.githubusercontent.com/davidjnevin/python_automation/main/emails.py
+curl -O https://raw.githubusercontent.com/davidjnevin/python_automation/main/report_email.py
+curl -O https://raw.githubusercontent.com/davidjnevin/python_automation/main/supplier_image_upload.py
+curl -O https://raw.githubusercontent.com/davidjnevin/python_automation/main/requirements.txt
+curl -O https://raw.githubusercontent.com/davidjnevin/python_automation/main/run.py
+curl -O https://https://raw.githubusercontent.com/davidjnevin/python_automation/main/health_check.py
+
